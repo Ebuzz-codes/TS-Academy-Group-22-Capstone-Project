d705adabcf9203213f2bb0babd2ca755af22be35
Ebube Okwuenu <okwuenue18@gmail.com>
2026-05-29 08:37:52 +0100

    Created 6 main components and updated App.jsx

---
+++ src/Components/header.jsx
@@ -0,0 +1,14 @@
+// Part 1: The Function (The "Factory")
+function header() {
+  
+  // Part 2: The Return (What the component actually looks like)
+  return (
+    <footer>
+      <p>© 2024 My Capstone Team. All rights reserved.</p>
+    </footer>
+  );
+}
+
+// Part 3: The Export (The "Delivery Label")
+// This is the most important line for Step 1!
+export default header;
