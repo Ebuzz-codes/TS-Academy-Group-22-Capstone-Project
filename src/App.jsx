894e4d293d9a8757421693a16e4993f3f491a479
gabstrong <freedomgabriel77@gmail.com>
2026-05-31 19:53:04 +0100

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
-import Header from './Components/header';
-import HeroSection from './Components/heroSection';
-import VideoSection from './Components/videoSection';
-import ImageSection from './Components/imageSection';
-import TableSection from './Components/tableSection';
-import FormSection from './Components/formSection';
-import Footer from './Components/footer';
+import Header from "./Components/header";
+import HeroSection from "./Components/HeroSection";
+import VideoSection from "./Components/videoSection";
+import ImageSection from "./Components/imageSection";
+import TableSection from "./Components/tableSection";
+import FormSection from "./Components/formSection";
+import Footer from "./Components/footer";
 
 function App() {
   return (
     <div>
-      <Header />
       <HeroSection />
       <VideoSection />
       <ImageSection />
@@ -18,7 +16,7 @@ function App() {
       <FormSection />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
