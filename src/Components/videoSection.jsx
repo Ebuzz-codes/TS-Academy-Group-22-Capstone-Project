a323475c9a601385427b9b05573d336608afd1c8
Ebube Okwuenu <okwuenue18@gmail.com>
2026-06-01 05:26:21 +0100

    Adjusted the className in the videosection

--- src/Components/videoSection.jsx
+++ src/Components/videoSection.jsx
@@ -1,18 +1,18 @@
-import styles from "../Components/videoSection.css";
+import "../Components/videoSection.css";
 import video from "../assets/Planet.mp4";
 
 export default function Astronomy() {
   return (
-    <div className={styles.bodyWrapper}>
+    <div className="bodyWrapper">
 
 {/* The second section */}
-        <section className={styles.section2}>
+        <section className="section2">
           <div>
             <video controls loop autoPlay muted>
               <source src={video} type="video/mp4" />
             </video>
           </div>
-          <div className={styles.section2text}>
+          <div className="section2text">
             <h2>How Planetary Data Helps Us Understand Space</h2>
             <p>
               Planetary science goes beyond images. Comparing{' '}
