50d9432ee2e15caddac234385f845dacd4c95c75
Ebube Okwuenu <okwuenue18@gmail.com>
2026-06-05 21:22:03 +0100

    Modified footer: added attribute

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -5,18 +5,17 @@ const Footer = () => {
     <footer className="footer">
       <div className="listnames">
         <h3>About</h3>
-        <p>This project was collaboratively designed and developed by:<strong> <a href="https://github.com/Ebuzz-codes">Ebube,</a> <a href="https://github.com/Gabstrong"> Freedom,</a> <a href="https://github.com/dimex002">Fawaz,</a> <a href="https://github.com/kennyfaola-coder">Kehinde,</a> <a href="https://github.com/Lasunkanmi26">Rachel,</a><a href="https://github.com/Denie-art">Emmanuella,</a> <a href="https://github.com/teenah11"> Augustina, </a> <a href="https://github.com/essalami">Simisola,</a></strong> combining our skills and expertise to deliver a functional and user-focused solution. </p>
+        <p>This project was collaboratively designed and developed by:<strong> <a href="https://github.com/Ebuzz-codes" target='_blank'>Ebube</a>, <a href="https://github.com/Gabstrong" target='_blank'> Freedom</a>, <a href="https://github.com/dimex002" target='_blank'>Fawaz</a>, <a href="https://github.com/kennyfaola-coder" target='_blank'>Kehinde</a>, <a href="https://github.com/Lasunkanmi26" target='_blank'>Rachel</a>, <a href="https://github.com/Denie-art" target='_blank'>Emmanuella</a>, <a href="https://github.com/teenah11" target='_blank'>Augustina</a>, <a href="https://github.com/essalami" target='_blank'>Simisola</a></strong>, combining our skills and expertise to deliver a functional and user-focused solution. </p>
       </div>
       
       <div className="about">
         <p className="links"> 
-          ©2026 Design by <a href="https://amakandukwu.com/">Amaka</a> &nbsp;&amp;&nbsp; 
-          <a href="https://www.linkedin.com/in/ifeomaokocha">Ifeoma A.</a> 
-          <a href="https://github.com/Ebuzz-codes/TS-Academy-Group-22-Capstone-Project.git"> TS Academy Phoenix Group 22.</a> All rights reserved
+          <a href="https://www.linkedin.com/in/ifeomaokocha" target='_blank'>Ifeoma A.</a> 
+          <a href="https://github.com/Ebuzz-codes/TS-Academy-Group-22-Capstone-Project.git" target='_blank'> TS Academy Phoenix Group 22.</a> All rights reserved
         </p>
 
         <p>
-          <a href="https://tsacademyonline.com/">TSAcademy</a>
+          <a href="https://tsacademyonline.com/" target='_blank'>TSAcademy</a>
         </p>
       </div>
     </footer>
