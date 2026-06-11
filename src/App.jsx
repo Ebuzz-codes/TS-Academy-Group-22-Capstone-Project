1dbae83cb003f09461ae6d64bb5c201bb7686bc1
Ebube Okwuenu <okwuenue18@gmail.com>
2026-06-11 19:54:29 +0100

    Fix import path for HeroSection component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Importing necessary components for the application
-import HeroSection from "./Components/heroSection";
+import HeroSection from "/src/Components/heroSection";
 import VideoSection from "./Components/videoSection";
 import PlanetSection from "./Components/imageSection";
 import TableSection from "./Components/tableSection";
